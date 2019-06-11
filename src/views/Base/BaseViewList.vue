be6a3b8a28ccb606ac9d7f71badf9c95d5e027f0
phamduykien <phamduykien@gmail.com>
2019-06-11 23:55:25 +0700

    sua loi code nham file BaseViewList

---
+++ src/views/Base/BaseViewList.vue
@@ -0,0 +1,22 @@
+<script>
+//Class base cho màn hình danh sách
+//CreatedBy: PDKIEN 11/06/2019
+export default {
+  name: "BaseViewList",
+  components: {},
+  props: {},
+  data: function() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    load: function() {},
+    refresh: function() {},
+    edit: function() {},
+    view: function() {
+      alert("x");
+    },
+    delete: function() {}
+  }
+};
+</script>
--- src/views/CA/CAList.vue
+++ src/views/CA/CAList.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <div>This is CAList</div>
-    <button @click="view"/>
+    <button @click="view" 
+    style="width:30px; height:20px"/>
     <MISAGrid/>
   </div>
 </template>
 
 <script>
-
 import BaseViewList from "@/views/Base/BaseViewList.vue";
 export default {
   extends: BaseViewList,
-  components: {
-   
-  }
+  methods: {
+    // view: function() {
+    //   alert("Extend");
+    // }
+  },
+  components: {}
 };
 </script>
 
